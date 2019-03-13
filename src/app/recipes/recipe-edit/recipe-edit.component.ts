dc551b5a72f2ae047d2dcd39599a2ddacc6e971c
Pavel Romanovsky <roman.pav.gen@gmail.com>
2019-03-13 18:32:49 +0300

    section 16

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute, Params} from '@angular/router';
+import {FormArray, FormControl, FormGroup, Validators} from '@angular/forms';
+import {RecipesService} from '../recipes.service';
+import {Recipe} from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -9,16 +12,75 @@ import {ActivatedRoute, Params} from '@angular/router';
 export class RecipeEditComponent implements OnInit {
   id: number;
   editMode = false;
+  recipeForm: FormGroup;
+  recipeIngredients = new FormArray([]);
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private recipeService: RecipesService) { }
 
   ngOnInit() {
     this.route.params.subscribe(
       (params: Params) => {
         this.id = +params['id'];
         this.editMode = params['id'] != null;
+        this.initForm();
       }
     );
+  }
+
+  private initForm() {
+    let recipeId;
+    let recipeName = '';
+    let recipeImagePath = '';
+    let recipeDescription = '';
+
+    if (this.editMode) {
+      const recipe = this.recipeService.getRecipe(this.id);
+      recipeId = this.id;
+      recipeName = recipe.name;
+      recipeImagePath = recipe.imagePath;
+      recipeDescription = recipe.description;
+      if (recipe['ingredients']) {
+        for(let ing of recipe.ingredients){
+          this.recipeIngredients.push(new FormGroup({
+            'id': new FormControl(ing.id),
+            'name': new FormControl(ing.name, Validators.required),
+            'amount': new FormControl(ing.amount, [Validators.required, Validators.pattern(/^[1-9]+[0-9]*$/)])
+          }));
+        }
+      }
+    }
+
+    this.recipeForm = new FormGroup({
+      'id': new FormControl(recipeId),
+      'name': new FormControl(recipeName, Validators.required),
+      'imagePath': new FormControl(recipeImagePath, Validators.required),
+      'description': new FormControl(recipeDescription, Validators.required),
+      'ingredients': this.recipeIngredients
+    });
+  }
+
+  onSubmit(){
+    console.log(this.recipeForm);
+    console.log(this.recipeForm.value);
+    if (this.editMode){
+      this.recipeService.updateRecipe(this.recipeForm.value);
+    }else{
+      this.recipeService.addRecipe(this.recipeForm.value);
+    }
+  }
+
+  getIngredientsControls() {
+    return (<FormArray>this.recipeForm.get('ingredients')).controls;
+  }
+
+  onAddIngredient(){
+    let newItem = new FormGroup({
+        'id': new FormControl(null),
+        'name': new FormControl(null, Validators.required),
+        'amount': new FormControl(null, [Validators.required, Validators.pattern(/^[1-9]+[0-9]*$/)])
+      }
+    );
+    this.recipeIngredients.push(newItem);
   }
 
 }
--- src/app/recipes/recipes.service.ts
+++ src/app/recipes/recipes.service.ts
@@ -2,13 +2,16 @@ import {Recipe} from './recipe.model';
 import {Injectable} from '@angular/core';
 import {Ingredient} from '../shared/ingredient.model';
 import {ShoppingListService} from '../shopping-list/shopping-list.service';
+import {Subject} from 'rxjs';
 
 @Injectable()
 export class RecipesService {
+  lastId = 2;
+  recipesChanged = new Subject<Recipe[]>();
 
   private recipes: Recipe[] = [
     new Recipe(
-      1,
+      0,
       'Shnitzel',
       'This is a test 1',
       'https://www.maxpixel.net/static/photo/1x/Mushrooms-Recipe-Kitchen-French-Dish-2459679.jpg',
@@ -17,7 +20,7 @@ export class RecipesService {
         new Ingredient(1, 'french fries', 2)
       ]),
     new Recipe(
-      2,
+      1,
       'Burger', 'This is a second recipe',
       'https://www.maxpixel.net/static/photo/1x/Mushrooms-Recipe-Kitchen-French-Dish-2459679.jpg',
       [
@@ -43,5 +46,24 @@ export class RecipesService {
 
   addIngredientsToShoppingList(ingredients: Ingredient[]) {
     this.shoppingListService.addIngredients(ingredients);
+  }
+
+  addRecipe(recipe: Recipe){
+    recipe.id = this.lastId++;
+    this.recipes.push(recipe);
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
+  updateRecipe(data: Recipe){
+    const recipe = this.recipes.find(
+      (s) => {
+        return s.id === data.id;
+      }
+    );
+    recipe.name = data.name;
+    recipe.imagePath = data.imagePath;
+    recipe.description = data.description;
+    recipe.ingredients = data.ingredients;
+    this.recipesChanged.next(this.recipes.slice());
   }
 }
